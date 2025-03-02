211d97d03b8f426de6bf8c1296614f624c696c3d
Darwvin <darwvin@hotmail.com>
2025-03-03 01:41:11 +0330

    .

--- date.util.js
+++ date.util.js
@@ -13,25 +13,26 @@ function convertToJalali(date) {
     return `${year}/${month}/${day}`;
 }
 
-function jalaliDateToGregorian(jalaliDate) {
-    const [year, month, day] = jalaliDate.split('/').map(Number);
+function jalaliToGregorian(jalaliDate) {
+    let parts = jalaliDate.split("/");
+    if (parts.length === 3) {
+        let jYear = parseInt(parts[0]);
+        let jMonth = parseInt(parts[1]);
+        let jDay = parseInt(parts[2]);
+        let gDate = new Date(jalali_to_gregorian(jYear, jMonth, jDay));
+        return gDate.toISOString().split('T')[0];
+    }
+    return '';
+}
 
-    const formatter = new Intl.DateTimeFormat('en-US-u-ca-persian', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-    });
-
-    const gregorianDate = new Date(
-        formatter.formatToParts(new Date(Date.UTC(year, month - 1, day)))
-            .find(part => part.type === 'year').value,
-        formatter.formatToParts(new Date(Date.UTC(year, month - 1, day)))
-            .find(part => part.type === 'month').value - 1,
-        formatter.formatToParts(new Date(Date.UTC(year, month - 1, day)))
-            .find(part => part.type === 'day').value
-    );
-
-    return gregorianDate;
+function jalali_to_gregorian(jy, jm, jd) {
+    let gy = jy + 621;
+    let days = (jm < 7) ? ((jm - 1) * 31) + jd : ((jm - 7) * 30) + jd + 186;
+    let gLeap = ((gy % 4 === 0 && gy % 100 !== 0) || (gy % 400 === 0)) ? 1 : 0;
+    let gYearDays = 365 + gLeap;
+    let gDate = new Date(gy, 0, 1);
+    gDate.setDate(gDate.getDate() + days - 1);
+    return gDate;
 }
 
 Date.prototype.addDays = function(days) {
--- date.util.php
+++ date.util.php
@@ -4,3 +4,42 @@ $fmt = new IntlDateFormatter('fa_IR@calendar=persian', IntlDateFormatter::SHORT,
 $today_jalali = $fmt->format(time());
 
 $today_jalali = str_replace(['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'], ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], $today_jalali);
+
+function gregorian_to_jalali($g_y, $g_m, $g_d, $delimiter = '/')
+{
+    $g_days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+    $j_days_in_month = array(31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29);
+
+    if ($g_y > 1600) {
+        $jy = 979;
+        $g_y -= 1600;
+    } else {
+        $jy = 0;
+        $g_y -= 621;
+    }
+
+    $gy2 = ($g_m > 2) ? ($g_y + 1) : $g_y;
+    $days = (365 * $g_y) + floor(($gy2 + 3) / 4) - floor(($gy2 + 99) / 100) + floor(($gy2 + 399) / 400) - 80 + $g_d;
+
+    for ($i = 0; $i < $g_m - 1; ++$i) {
+        $days += $g_days_in_month[$i];
+    }
+
+    $jy += 33 * floor($days / 12053);
+    $days %= 12053;
+    $jy += 4 * floor($days / 1461);
+    $days %= 1461;
+
+    if ($days > 365) {
+        $jy += floor(($days - 1) / 365);
+        $days = ($days - 1) % 365;
+    }
+
+    for ($i = 0; ($days >= $j_days_in_month[$i]) && ($i < 11); ++$i) {
+        $days -= $j_days_in_month[$i];
+    }
+    $jm = $i + 1;
+    $jd = $days + 1;
+
+    return $jy . $delimiter . str_pad($jm, 2, '0', STR_PAD_LEFT) . $delimiter . str_pad($jd, 2, '0', STR_PAD_LEFT);
+}
